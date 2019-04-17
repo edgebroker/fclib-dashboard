d2b368d8aaf09668a79617cbeade3e0fd4e432f7
iitsoftware <am@iit.de>
2019-04-17 03:19:56 +0200

    Allow to reference {body} of the incoming text message

--- Dashboard/Services/Shell Reply/links/input/In/handler.js
+++ Dashboard/Services/Shell Reply/links/input/In/handler.js
@@ -11,7 +11,7 @@ function handler(In) {
             message: null
         }
     };
-    var text = this.flowcontext.substitute(this.props["message"]);
+    var text = replaceAll(this.flowcontext.substitute(this.props["message"], "{body}", In.body()));
 
     In.properties().forEach(function(prop){
         text = replaceAll(text, "\\{"+prop.name()+"\\}", prop.value().toString());
