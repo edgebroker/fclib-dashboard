18727896509b5f682313910addc89005bd044ad3
iitsoftware <am@iit.de>
2019-09-26 17:31:31 +0200

    fix

--- Dashboard/Probes/Joins/Join Two/links/input/One/handler.js
+++ Dashboard/Probes/Joins/Join Two/links/input/One/handler.js
@@ -2,5 +2,5 @@ function handler(One) {
     if (this.props["accumulate"] === true)
         this.cnt1 += One.body.values[0];
     else
-        this.cnt2 = One.body.values[0];
+        this.cnt1 = One.body.values[0];
 }
