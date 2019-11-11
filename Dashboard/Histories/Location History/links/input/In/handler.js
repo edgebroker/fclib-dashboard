463685417d18e1f16dfe667b213190feaf9ef849
iitsoftware <am@iit.de>
2019-11-11 16:48:52 +0100

    bug fix

--- Dashboard/Histories/Location History/links/input/In/handler.js
+++ Dashboard/Histories/Location History/links/input/In/handler.js
@@ -1,7 +1,5 @@
 function handler(In) {
     this.assertProperty(In, this.props["assetproperty"]);
-    this.assertProperty(In, this.props["longproperty"]);
-    this.assertProperty(In, this.props["latproperty"]);
     if (this.props["eventtimeproperty"])
         this.assertProperty(In, this.props["eventtimeproperty"]);
 
